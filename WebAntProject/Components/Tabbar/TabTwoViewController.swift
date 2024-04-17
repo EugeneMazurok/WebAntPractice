733dc22e0b0196ca7c8dec48a305bfc40d90d169
EugeneMazurok <148581786+EugeneMazurok@users.noreply.github.com>
2024-04-17 18:28:08 +0300

    Update TabTwoViewController.swift

--- WebAntProject/Components/Tabbar/TabTwoViewController.swift
+++ WebAntProject/Components/Tabbar/TabTwoViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class TabTwoViewController: UIViewController {
+    private var photoGridViewController: PhotoGridViewController!
+    private var collectionViewLayout: UICollectionViewFlowLayout!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.isNavigationBarHidden = true
-        self.title = "Popular"
-        self.view.backgroundColor = UIColor.green
+        title = "Popular"
+        view.backgroundColor = .blue
+
+        let photoGridViewController = PhotoGridViewController(photos: [], collectionViewLayout: UICollectionViewFlowLayout(), photoType:.popular)
+        let navigationController = UINavigationController(rootViewController: photoGridViewController)
+        addChild(navigationController)
+        view.addSubview(navigationController.view)
+        navigationController.didMove(toParent: self)
+        navigationController.view.frame = view.bounds
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 }
